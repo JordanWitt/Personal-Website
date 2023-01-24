56bb03faaaab219a646d0a1555347c74c2ba71b8
JordanWitt <wittjordan94@gmail.com>
2023-01-24 12:40:08 -0600

    added a dark mode feature

--- js/home.js
+++ js/home.js
@@ -13,11 +13,37 @@ let paragraghOne = document.getElementById("paragraph-one");
 let paragraghTwo = document.getElementById("paragraph-two");
 let paragraghThree = document.getElementById("paragraph-three");
 
+//STYLING CSS DARK MODE
+let body = document.getElementById("body");
+let border = document.getElementById("border");
+let moon = document.getElementById("moon");
+let marquee = document.getElementById("marqueeText");
+let darkMode = false;
+
+
+moon.addEventListener("click", function () {
+    darkMode = !darkMode;
+    if (darkMode) {
+        body.classList.add("dark-mode");
+        projectDisplay.classList.add("dark-mode-shadow");
+        border.classList.add("dark-mode-border");
+        projectOne.classList.add("dark-mode-a:hover")
+        marquee.classList.add("dark-mode-marqueeText");
+        moon.classList.add("dark-mode-moon");
+    } else {
+        body.classList.remove("dark-mode");
+        projectDisplay.classList.remove("dark-mode-shadow");
+        border.classList.remove("dark-mode-border");
+        projectOne.classList.remove("dark-mode-a:hover")
+        marquee.classList.remove("dark-mode-marqueeText");
+        moon.classList.remove("dark-mode-moon");
+    }
+});
 
 //EVENT LISTENERS FOR HOME PAGE
 jordanImg.addEventListener("mouseover", function (){
     projectDisplay.src = "images/jordan.jpeg";
-    paragraghOne.innerText = "Howdy,"
+    paragraghOne.innerText = "Hello There,"
     paragraghTwo.innerText = " First off my name is Jordan and here is a lil' bit about myself: \n\n I'll try my best to keep this short. I struggled for many years trying to find a career path that I felt best suit me. I like a little bit of art, a little bit of logic..and..BOOM...coding. That made it sound simple...it wasn't. I was recommended a bootcamp by a friend and before you know it I was coding away with Codeup(the bootcamp). I never thought I could love and hate doing something simultaneously but here we are! With coding I am able to satisfy my artistic needs with front-end development as well as my logical needs i.e: problem solving and debugging; the best of both worlds. I could write more but for the sake of keeping it short I'll just end it here. Enjoy!"
     paragraghThree.innerText = "Fun Facts:\n\n" + "1. My favorite color is yellow\n" + "2. I am obsessed with Panda Bears\n" + "3. I can't stand onions(I tried, I promise!)"
 });
