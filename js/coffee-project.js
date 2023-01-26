76f7c5a04e0a7082735c4e4a061816f1a7f8e542
JordanWitt <wittjordan94@gmail.com>
2023-01-26 16:57:00 -0600

    stuck, creating wireframes

---
+++ js/coffee-project.js
@@ -0,0 +1,15 @@
+$( "#resize" ).click( function() {
+
+    $( "#pink-line" ).animate( {
+
+        width: '-=30px'
+
+    }, 1000 );
+
+} );
+
+$( '#code' ).click( function() {
+
+    $( '.trigger' ).toggle( 'slow' );
+
+} );
